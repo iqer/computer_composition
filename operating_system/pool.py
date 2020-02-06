e330f13e874553f90be1f45446650c61295e61fb
ArthurChen <cqh0718@gmail.com>
2020-02-06 21:19:04 +0800

    add pool

--- operating_system/pool.py
+++ operating_system/pool.py
@@ -1,7 +1,9 @@
 # -*- encoding:utf-8 -*-
 
+import psutil
 import threading
 from operating_system.task import Task
+from operating_system.ThreadSafeQueue import ThreadSafeQueue
 
 
 class ProcessThread(threading.Thread):
@@ -27,3 +29,42 @@ class ProcessThread(threading.Thread):
 
     def stop(self):
         self.dismiss()
+
+
+class ThreadPool:
+
+    def __init__(self, size=0):
+        if not size:
+            self.size = psutil.cpu_count() * 2
+        self.task_queue = ThreadSafeQueue(self.size)
+        self.pool = ThreadSafeQueue()
+        for i in range(size):
+            self.pool.put(ProcessThread(self.task_queue))
+
+    def start(self):
+        for i in range(self.size()):
+            thread = self.pool.get(i)
+            thread.run()
+
+    def join(self):
+        for i in range(self.size()):
+            thread = self.pool.get(i)
+            thread.stop()
+
+    def put(self, item):
+        if not isinstance(item, Task):
+            raise TaskTypeError()
+        self.task_queue.put(item)
+
+    def batch_put(self, item_list):
+        if not isinstance(item_list, list):
+            item_list = list(item_list)
+        for item in item_list:
+            self.put(item)
+
+    def size(self):
+        return self.pool.size()
+
+
+class TaskTypeError(Exception):
+    pass
